025abe4b657431f597ab1b3ccd67ad47f09fcdb3
sunilnahata <sunilnahata4u@gmail.com>
2024-03-21 19:39:29 +0100

    hello.py passes all tests

--- 01_hello/hello.py
+++ 01_hello/hello.py
@@ -1,3 +1,26 @@
 #!/usr/bin/python
-# Say hello
-print("Hello, World!")
+"""
+Author: Sunil Nahata
+Purpose: Say hello
+"""
+import argparse
+
+
+def get_args():
+    """ "Get the command-line arguments"""
+    parser = argparse.ArgumentParser(description="Say hello")
+    parser.add_argument(
+        "-n", "--name", metavar="name", default="World", help="Name to greet"
+    )
+    return parser.parse_args()
+
+
+def main():
+    """main"""
+    args = get_args()
+    name = args.name
+    print(f"Hello, {name}!")
+
+
+if __name__ == "__main__":
+    main()
